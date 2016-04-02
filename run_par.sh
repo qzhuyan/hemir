3cfe1667766ef6c40e23a4b938260a7f703aaa8f
William Yang <mscame@gmail.com>
2016-04-02 21:44:50 +0200

    better job control

--- run_par.sh
+++ run_par.sh
@@ -1,33 +1,45 @@
 #!/bin/bash 
 WORKERS=${1:-2}
-homes=(`casperjs ./hemmirror.js`)
-num=${#homes[@]}
-echo "found $num homes";
 
 worker() {
     home=$1
     echo "$(date) fetch home $home"
     res=$(casperjs ./h2b.js --url=$home);
     url=$(echo $res |  awk -F '|' '{print $3}')
-    echo "broker url is $url"
-    timeout 60 casperjs ./broker.js --url=$url    
+    if [ "null" == $url ];
+    then
+	echo "broker url is $url, skipp.."
+    else
+	echo "broker url is $url"
+	timeout 60 casperjs ./broker.js --url=$url
+    fi
 }
 
 test_job() {
-   sleep 5
+   sleep 5 &
 }
 
-bgjobs=()
+wait_for_worker()
+{
+    bgs=$(jobs -p |grep -v Done | wc -l)
+    if [ $bgs -ge $WORKERS ];
+    then
+       echo "$bgs workers in bg, wait ..."
+       sleep 1;
+       wait_for_worker
+    else
+	echo "$bgs workers in bg, conti.."
+    fi
+}
+
+homes=(`casperjs ./hemmirror.js`)
+num=${#homes[@]}
+echo "found $num homes";
+
 for h in "${homes[@]}";
 do
-    if [ ${#bgjobs[@]} -eq $WORKERS ];
-    then
-	echo 'wait... '
-	wait ${bgjobs[0]}
-	bgjobs=(${bgjobs[@]:1})
-    else
-	worker $h &
-	bgjobs+=($!)
-    fi
+    echo "handling $h"
+    wait_for_worker
+    worker $h &
 done
 
